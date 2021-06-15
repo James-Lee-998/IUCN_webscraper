a0237fa0d284b5d545f25bf98e758ba389f0dc97
hongkongnoodles <tonghan100@gmail.com>
2021-06-15 08:58:19 +0100

    Use dplyr to optomise code

---
+++ GBIF.R
@@ -0,0 +1,5 @@
+install.packages('rgbif')
+library(rgbif)
+
+List_of_Species = read.csv('MexTMF_test.csv')
+
--- R_code_access_API.R
+++ R_code_access_API.R
@@ -6,12 +6,12 @@ suppressMessages(library(rlist))
 suppressMessages(library(reshape2))
 suppressMessages(library(ggplot2))
 suppressMessages(library(Rmisc))
-
+suppressMessage(library(dplyr))
+suppressMessage(library(plotly))
 ####################################API and requests############################################
 ################################################################################################
 
 #Obtain IUCN API key 
-Sys.getenv("IUCN_KEY") # hides IUCN API key in R environment
 apikey <- Sys.getenv("IUCN_KEY") # Obtains API key from environment and sets it to callable variable
 
 #Gorilla test
@@ -26,63 +26,44 @@ all_df <- out$result # obtain results only
 ################################################################################################
 
 # obtain all endangered species quoted isocodes are indicative of endangered or vulnerable categories
-all_df = all_df[(all_df$category == 'CR' | all_df$category == 'EN' | all_df$category == 'VU' | all_df$category == 'LR'),]
-Endangered_list = as.data.frame(all_df$scientific_name) # primes the species list so that we only see species which are vulnerable
-colna = "Species"
-colnames(Endangered_list) = colna
 
-df <- Endangered_list %>%  # iterate through endangered species list and obtain their respective habitats
-  mutate(iucn_pull = map(Species, rl_habitats, key = apikey))
+Endangered_list <- all_df %>% 
+  filter(category == c('CR','EN', 'VU', 'LR')) %>% 
+  mutate(iucn_pull = map(scientific_name, rl_habitats, key = apikey))
 
-api_clean <- df %>% # cleans the data so we obtain only the habitats
+api_clean <- Endangered_list %>% 
   mutate(habitat = map(iucn_pull, pluck, "result", "habitat")) %>% 
-  select(Species, habitat)
-
-# creates a new logic column in the dataframe, where by true refers to forest - subtropical/tropical montane habitats
-api_clean['BOOLEAN'] = list(sapply(api_clean$habitat, `%in%`, x = 'Forest - Subtropical/Tropical Moist Montane'))
-
-# obtains all true values returning all scientific names for montane species
-api_clean_true = api_clean[(api_clean[3] == 'TRUE'),]
-
-# search through all species lists once again to obtain narrative information
-df_complete = api_clean_true %>%
-  mutate(iucn_pull_2 = map(Species, rl_search, key = apikey))
-
-final_df <- df_complete %>% # cleans the data so we obtain only elevation_lower
-  mutate(elevation_lower = map(iucn_pull_2, pluck, "result", "elevation_lower")) %>% 
-  select(Species, elevation_lower)
-
-final_df_2 <- df_complete %>% # cleans the data so we obtain only elevation_lower
-  mutate(endangered = map(iucn_pull_2, pluck, "result", "category")) %>% 
-  select(Species, endangered)
+  select(scientific_name, habitat) %>%
+  mutate(BOOLEAN = map(habitat, `%in%`, x = 'Forest - Subtropical/Tropical Moist Montane')) %>%
+  filter(BOOLEAN == 'TRUE') %>%
+  select(scientific_name) %>%
+  mutate(iucn_pull_2 = map(scientific_name, rl_search, key = apikey)) %>%
+  mutate(elevation_lower = as.numeric(map(iucn_pull_2, pluck, "result", "elevation_lower"))) %>%
+  mutate(category = as.character(map(iucn_pull_2, pluck, "result", "category"))) %>% 
+  select(scientific_name, elevation_lower, category) %>%
+  filter(elevation_lower > 1000)
 
 ###################sort list so we only get species with an elevation_lower of 1000m###############
 ###################################################################################################
 
-final_df = cbind(final_df,final_df_2)
-final_df = final_df[(!final_df$elevation_lower == 'NA'),]
-final_df = final_df[(!final_df$elevation_lower < 1000),]
-final_df = final_df %>% select(-3)
+write.csv(api_clean, "D:\\ENDANGERED_MALAYSIA_ELEVATION_1000.csv", row.names = FALSE)
 
-final_df['elevation'] = unlist(final_df$elevation_lower)
-final_df['category'] = unlist(final_df$endangered)
-final_df = final_df %>% select(-elevation_lower & -endangered)
+model_1 = aov(elevation_lower~category, data = api_clean)
 
-ggplot(final_df, aes(x = Species, y = elevation, color = category)) + geom_point() + 
-  theme_bw() + 
-  theme(axis.text.x = element_blank(), axis.ticks.x = element_blank()) +
-  scale_y_continuous(limits = c(1000,NA), expand = c(1000,NA))
+shapiro.test(resid(aov(elevation_lower~category, data = api_clean)))
 
-write.csv(final_df, "D:\\ENDANGERED_MALAYSIA_ELEVATION_1000.csv", row.names = FALSE)
+model_2 = kruskal.test(elevation_lower~category, data = api_clean)
 
-x = aov(elevation~category, data = final_df)
-summary(x)
+model_2
 
-tgc <- summarySE(final_df, measurevar="elevation", groupvars="category")
+tgc <- summarySE(api_clean, measurevar="elevation_lower", groupvars="category")
 
-ggplot(final_df, aes(x = category, y = elevation, fill = category)) + geom_boxplot() + 
-  geom_jitter(width = 0.1) + 
+plot_1 = ggplot(api_clean, aes(x = category, y = elevation_lower, fill = category, label = scientific_name)) + 
+  geom_jitter(width = 0.1) +
+  geom_boxplot() + 
   scale_fill_discrete(labels = c("Critically Endangered", "Endagered", "Vulnerable")) + 
   scale_y_continuous(limits = c(0,3500), expand = c(0,NA)) +
   labs(y = "Lower elevation /m", x = " ", fill = "IUCN category") + 
   theme_bw()
+
+ggplotly(plot_1)
